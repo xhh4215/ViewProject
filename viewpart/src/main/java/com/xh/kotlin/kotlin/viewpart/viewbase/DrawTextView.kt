cc7e27f129cbffb63e4691a7d5bea8da964b28b5
xhh <18734184215@163.com>
2020-06-24 20:53:33 +0800

    使用贝塞尔曲线实现签字板的功能

--- viewpart/src/main/java/com/xh/kotlin/kotlin/viewpart/viewbase/DrawTextView.kt
+++ viewpart/src/main/java/com/xh/kotlin/kotlin/viewpart/viewbase/DrawTextView.kt
@@ -12,6 +12,9 @@ import android.view.View
 class DrawTextView : View {
     val path = Path()
     val paint = Paint()
+    var mPreX: Float = 0f
+    var mPreY: Float = 0f
+
 
     constructor(context: Context, attributeSet: AttributeSet) : super(context, attributeSet)
 
@@ -19,12 +22,22 @@ class DrawTextView : View {
     override fun onTouchEvent(event: MotionEvent): Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
+                //设置手指刚接触的位置为贝塞尔曲线的起始点
                 path.moveTo(event.x, event.y)
+                //手指刚刚碰到的时候的坐标点
+                mPreX = event.x
+                mPreY = event.y
                 return true
             }
             MotionEvent.ACTION_MOVE -> {
-                path.lineTo(event.x, event.y)
-                postInvalidate()
+                //设置贝塞尔曲线的结束点
+                val endx = (mPreX+event.x)/2
+                val endy = (mPreY+event.y)/2
+                //设置贝塞尔曲线控制点和结束点
+                path.quadTo(mPreX,mPreY,endx, endy)
+                mPreX = event.x
+                mPreY = event.y
+                invalidate()
                 return true
 
             }
@@ -36,6 +49,7 @@ class DrawTextView : View {
         super.onDraw(canvas)
         paint.color = Color.RED
         paint.style = Paint.Style.STROKE
+        paint.strokeWidth = 30f
         canvas.drawPath(path, paint)
     }
 
