3d1457dc037f9ec2019bbfaae1bff5f31599c7b5
xhh <18734184215@163.com>
2020-06-26 12:56:20 +0800

    自定以的View为图片添加阴影的实现

--- viewpart/src/main/java/com/xh/kotlin/kotlin/viewpart/viewbase/ExtractAlphaView.kt
+++ viewpart/src/main/java/com/xh/kotlin/kotlin/viewpart/viewbase/ExtractAlphaView.kt
@@ -10,13 +10,19 @@ class ExtractAlphaView : View {
     var paint = Paint()
     lateinit var bitmap: Bitmap
     lateinit var alphaBitmap: Bitmap
+    private var srcBitmap = 0
+    private var shadowX = 0
+    private var shadowY = 0
+    private var shadowColor = 0
+    private var shadowRedius = 0f
+
 
     constructor(context: Context) : super(context) {
-        init()
     }
 
     constructor(context: Context, attributeSet: AttributeSet) : super(context, attributeSet) {
-        init()
+
+        init(context, attributeSet)
     }
 
     constructor(context: Context, attributeSet: AttributeSet, defstyle: Int) : super(
@@ -24,25 +30,48 @@ class ExtractAlphaView : View {
         attributeSet,
         defstyle
     ) {
-        init()
+        init(context, attributeSet)
 
     }
 
-    fun init() {
+    fun init(context: Context, attributeSet: AttributeSet) {
         setLayerType(LAYER_TYPE_SOFTWARE, null)
-        bitmap = BitmapFactory.decodeResource(this.resources, R.drawable.smallpz)
+        val intArray = context.obtainStyledAttributes(attributeSet, R.styleable.ExtractAlphaView)
+        srcBitmap = intArray.getResourceId(R.styleable.ExtractAlphaView_src, R.drawable.smallpz)
+        shadowX = intArray.getInt(R.styleable.ExtractAlphaView_shadowX, 0)
+        shadowY = intArray.getInt(R.styleable.ExtractAlphaView_shadowY, 0)
+        shadowColor = intArray.getInt(R.styleable.ExtractAlphaView_shadowColor, R.color.colorAccent)
+        shadowRedius = intArray.getFloat(R.styleable.ExtractAlphaView_shadowRadius, 10f)
+        intArray.recycle()
+        bitmap = BitmapFactory.decodeResource(this.resources, srcBitmap)
         // 新建一张仅具有Alpha值的空白图像
         alphaBitmap = bitmap.extractAlpha()
     }
 
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        val measureWidth = MeasureSpec.getSize(widthMeasureSpec)
+        val measureHeight = MeasureSpec.getSize(heightMeasureSpec)
+        val measureWidthModel = MeasureSpec.getMode(widthMeasureSpec)
+        val measureHeightModel = MeasureSpec.getMode(heightMeasureSpec)
+        val width = bitmap.width
+        val height = bitmap.height
+        /***
+         * wrap_content --->MeasureSpec.AT_MOST
+         * match_parent ---> MeasureSpec.EXACTLY
+         * 具体值 -> MeasureSpec.EXACTLY
+         */
+        setMeasuredDimension(if(measureWidthModel== MeasureSpec.EXACTLY)measureWidth else width,if(measureHeightModel==MeasureSpec.EXACTLY)measureHeight else height)
+    }
+
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
-        paint.color = Color.RED
-        paint.maskFilter = BlurMaskFilter(10f,BlurMaskFilter.Blur.NORMAL)
-        canvas.drawBitmap(alphaBitmap,null, Rect(100,100,300,300),paint)
+        paint.color = shadowColor
+        paint.maskFilter = BlurMaskFilter(shadowRedius, BlurMaskFilter.Blur.NORMAL)
+        canvas.drawBitmap(alphaBitmap, null, Rect(90 + shadowX, 90 + shadowY, 300, 300), paint)
 
         paint.maskFilter = null
-        canvas.drawBitmap(bitmap,null,Rect(90,90,290,290),paint)
+        canvas.drawBitmap(bitmap, null, Rect(90, 90, 290, 290), paint)
 
     }
 }
